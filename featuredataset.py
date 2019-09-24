34ec48e3533c27ea504a22a32d3dfdf4794755c1
rabjohnk <krabjohn@flemingcollege.ca>
2019-09-24 14:13:57 -0400

    sept24

--- featuredataset.py
+++ featuredataset.py
@@ -8,9 +8,10 @@ Created on Tue Jan 29 14:01:18 2019
 
 # Import system modules
 import arcpy
+from arcpy import env
 
 # Set environment settings
-arcpy.env.workspace = "h:/data
+env.workspace = "h:/data"
  
 # Set local variables
 inFeatures = "schools"
@@ -21,24 +22,25 @@ fieldName2 = "status"
 fieldLength = 10
 
 # Set local variables
-out_dataset_path = "h:/data/greenspace.gdb" 
-out_name = "trees"
+out_dataset_path = "h:/data/brownspace2.gdb" 
+FDC_name = "Growth"
+FC_Name = "TREES"
 
 # Creating a spatial reference object nad 83 utm zone 17 n
 sr = arcpy.SpatialReference(26917)
 
 # Create a FileGDB for the fds
-#arcpy.CreateFileGDB_management("h:/data", "greenspace.gdb")
+arcpy.CreateFileGDB_management("h:/data", "brownspace2.gdb")
 
 # Execute CreateFeaturedataset 
-arcpy.CreateFeatureDataset_management(out_dataset_path, out_name, sr)
+arcpy.CreateFeatureDataset_management(out_dataset_path, FDC_name, sr)
 
+arcpy.CreateFeatureclass_management(out_dataset_path, FC_Name, "POINT")
+# CreateFeatureclass_management (out_path, out_name, {geometry_type}, {template}, {has_m}, {has_z}, {spatial_reference}, {config_keyword}, {spatial_grid_1}, {spatial_grid_2}, {spatial_grid_3})
  
-
- 
-# Execute AddField twice for two new fields
-arcpy.AddField_management(inFeatures, fieldName1, "LONG", fieldPrecision,
-                          field_alias=fieldAlias, field_is_nullable="NULLABLE")
+# # Execute AddField twice for two new fields
+# arcpy.AddField_management(inFeatures, fieldName1, "LONG", fieldPrecision,
+#                           field_alias=fieldAlias, field_is_nullable="NULLABLE")
                           
-arcpy.AddField_management(inFeatures, fieldName2, "TEXT", field_length=fieldLength)
+# arcpy.AddField_management(inFeatures, fieldName2, "TEXT", field_length=fieldLength)
 
